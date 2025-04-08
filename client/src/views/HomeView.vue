40547f974fe805c29513715e77a0e1c7c3290e20
Irfan Tahir <irfpann@student.usm.my>
2025-04-08 23:00:05 +0800

    first commit

---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Button from '@/volt/Button.vue'
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center min-h-screen text-center">
+    <h1 class="text-5xl font-bold p-3">Your Application</h1>
+    <Button label="Hello World" />
+  </div>
+</template>
+
+<style scoped></style>
